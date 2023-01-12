c1ca5bfd90a0835e57ea973fdd13a1e6ef686619
ntet2001 <manyamaigor2001@gmail.com>
2023-01-12 21:28:35 +0100

    first commit

---
+++ uberEat-clone/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Debut du Projet Uber Eats Raoul</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
