9df44a3f03502f995cef27c78c29d07911c6a118
ntet2001 <manyamaigor2001@gmail.com>
2023-01-13 04:28:07 +0100

    restaurant item created

--- uberEat-clone/App.js
+++ uberEat-clone/App.js
@@ -1,11 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+
+// {RestaurantItem component} 
+
+const RestaurantItem = () => {
+  return (
+    <View style={styles.restaurantContainer}>
+      <Image
+        source={{
+          uri: "https://notjustdev-dummy.s3.us-east-2.amazonaws.com/uber-eats/restaurant2.jpeg"
+        }}
+        style={styles.image}
+      />
+      <Text style={styles.title}>El Cabo Coffe Bar Tres De Mayo</Text>
+      <Text style={styles.subtitle}>$ 1.40  25-35 min</Text>
+    </View>
+  )
+};
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Debut du Projet Uber Eats Raoul</Text>
-      <StatusBar style="auto" />
+      {/* {restaurant views} */}
+      <RestaurantItem/>
+      <RestaurantItem/>
     </View>
   );
 }
@@ -16,5 +34,24 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  restaurantContainer:{
+    width: '100%',
+    padding: 10,
+    marginVertical: 2,
+  },
+  image: {
+    width: '100%',
+    aspectRatio: 4 /3,
+  },
+  title: {
+    marginTop:5,
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  subtitle: {
+    marginTop:5,
+    fontSize: 15,
+    color: 'gray',
   },
 });
